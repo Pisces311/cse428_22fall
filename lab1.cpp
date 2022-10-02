94a73a63eaeb4d7aeb93d8c92829048fceeb1401
Copy of Mavis's Brain <58226449+Mavis-H@users.noreply.github.com>
2022-10-02 18:48:12 -0500

    Finished lab1 part 14, 15 and 16

--- lab1.cpp
+++ lab1.cpp
@@ -1,35 +1,49 @@
 #include "PinochleGame.h"
 #include "HoldEmGame.h"
 
-const int SUCCESS = 0;
+using std::shared_ptr;
+using std::make_shared;
+
+const int COMMAND_LINE_ARG_ERROR = 1;
+const int CREATE_GAME_OBJECT_ERROR = 2;
+const int GAME_NAME_INDEX = 1;
+const int PINOCHLE_PLAYER_NUM = 4;
+const int HOLDEM_PLAYER_NUM_MIN = 2;
+const int HOLDEM_PLAYER_NUM_MAX = 9;
+
+shared_ptr<Game> create(int argc, const char **argv) {
+    shared_ptr<Game> spGame;
+    std::string gameName = argv[GAME_NAME_INDEX];
+
+    if (gameName == "Pinochle") {
+        spGame = make_shared<PinochleGame>(argc, argv);
+    }
+    else if (gameName == "HoldEm")
+    {
+        spGame = make_shared<HoldEmGame>(argc, argv);
+    }
+
+    return spGame;
+}
 
 int main(int argc, const char **argv) {
-    // HoldEmDeck holdEmDeck;
-    // PinochleDeck pinochleDeck1;
-    // PinochleDeck pinochleDeck2;
-
-//    exercise 9 test
-//    holdEmDeck.shuffle();
-//    holdEmDeck.print(std::cout, 13);
-//     pinochleDeck.print(std::cout, 6);
-
-//    exercise 9 test
-//    pinochleDeck1.print(std::cout, 10);
-//    pinochleDeck2.print(std::cout, 10);
-//    pinochleDeck1 >> pinochleDeck2;
-//    pinochleDeck1.print(std::cout, 10);
-//    pinochleDeck2.print(std::cout, 10);
-
-//    exercise 9 test
-//    pinochleDeck1.print(std::cout, 10);
-//    pinochleDeck2.print(std::cout, 10);
-//    pinochleDeck1.collect(pinochleDeck2);
-//    pinochleDeck1.print(std::cout, 10);
-//    pinochleDeck2.print(std::cout, 10);
-
-    // PinochleGame game(argc, argv);
-    HoldEmGame game(argc, argv);
-    game.play();
-
-    return SUCCESS;
+    // Check command line arguments
+    std::string gameName = argv[GAME_NAME_INDEX];
+    if ((gameName == "Pinochle" && argc == 2 + PINOCHLE_PLAYER_NUM) || 
+        (gameName == "HoldEm" && argc >= 2 + HOLDEM_PLAYER_NUM_MIN && argc <= 2 + HOLDEM_PLAYER_NUM_MAX)) {
+        shared_ptr<Game> spGame = create(argc, argv);
+        if (spGame) {
+            return spGame -> play();
+        }
+        else {
+            std::cout << "Creat game object failed." << std::endl;
+            return CREATE_GAME_OBJECT_ERROR;
+        }
+    }
+    else {
+        std::cout << "Usage: ./lab1 Pinochle/HoldEm player1 player2 ..." << std::endl;
+        std::cout << "[Pinochle]: must have 4 players" << std::endl;
+        std::cout << "[HoldEm]: between 2 and 9 players" << std::endl;
+        return COMMAND_LINE_ARG_ERROR;
+    }
 }
